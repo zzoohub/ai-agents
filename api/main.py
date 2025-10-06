4904e82a13316312dda35c7e2d290f9658b66cbf
YOUNGSEOK JOO <zzoo@YOUNGSEOKs-MacBook-Pro.local>
2025-10-06 17:17:10 +0900

    chore: setting chatgpt-clone

--- api/main.py
+++ api/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from api!")
+import os
+import dotenv
 
+from openai import OpenAI
 
-if __name__ == "__main__":
-    main()
+dotenv.load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Explain to me how AI works"
+        }
+    ]
+)
+
+message = response.choices[0].message
---
+++ chatgpt-clone/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from chatgpt-clone!")
+
+
+if __name__ == "__main__":
+    main()
