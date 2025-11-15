4d363bf3ff4fdd40d9ea91125f37dbdd19b47a52
YOUNGSEOK JOO <zzoo@YOUNGSEOKs-MacBook-Pro.local>
2025-11-15 23:42:27 +0900

    feat: streamlit

--- chatgpt-clone/main.py
+++ chatgpt-clone/main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from chatgpt-clone!")
+import asyncio
 
+import streamlit as st
+from agents import Agent, Runner, SQLiteSession
 
-if __name__ == "__main__":
-    main()
+if "session" not in st.session_state:
+    st.session_state["session"] = SQLiteSession(
+        "chat-history", "chatgpt-clone-memory-db"
+    )
+
+session = st.session_state["session"]
+with st.sidebar:
+    reset = st.button("Reset")
+    if reset:
+        asyncio.run(session.clear_session())
+    st.write(asyncio.run(session.get_items()))
+
+prompt = st.chat_input("Write a message for your assistant")
+
+if prompt:
+    with st.chat_message("human"):
+        st.write(prompt)
