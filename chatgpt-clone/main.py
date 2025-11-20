f263567ed76d45c92385a1ba4a8319ed24100e8c
YOUNGSEOK JOO <zzoo@YOUNGSEOKs-MacBook-Pro.local>
2025-11-21 03:58:58 +0900

    feat: connect openai api and basic function that chat to ai model

--- chatgpt-clone/main.py
+++ chatgpt-clone/main.py
@@ -1,7 +1,20 @@
 import asyncio
 
+import dotenv
 import streamlit as st
 from agents import Agent, Runner, SQLiteSession
+
+dotenv.load_dotenv()
+
+if "agent" not in st.session_state:
+    st.session_state["agent"] = Agent(
+        name="ChatGPT Clone",
+        instructions="""
+        You are a helpful assistant.
+        """,
+    )
+
+agent = st.session_state["agent"]
 
 if "session" not in st.session_state:
     st.session_state["session"] = SQLiteSession(
@@ -9,14 +22,26 @@ if "session" not in st.session_state:
     )
 
 session = st.session_state["session"]
-with st.sidebar:
-    reset = st.button("Reset")
-    if reset:
-        asyncio.run(session.clear_session())
-    st.write(asyncio.run(session.get_items()))
+
+
+async def run_agent(message: str):
+    stream = Runner.run_streamed(agent, message, session=session)
+    async for event in stream.stream_events():
+        if event.type == "raw_response_event":
+            if event.data.type == "response.output_text.delta":
+                with st.chat_message("ai"):
+                    st.write(event.data.delta)
+
 
 prompt = st.chat_input("Write a message for your assistant")
 
 if prompt:
     with st.chat_message("human"):
         st.write(prompt)
+    asyncio.run(run_agent(prompt))
+
+with st.sidebar:
+    reset = st.button("Reset")
+    if reset:
+        asyncio.run(session.clear_session())
+    st.write(asyncio.run(session.get_items()))
