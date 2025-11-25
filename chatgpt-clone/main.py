23e2cf119625202678226ffe4f7026037376e95d
YOUNGSEOK JOO <zzoo@YOUNGSEOKs-MacBook-Pro.local>
2025-11-26 05:31:41 +0900

    feat: ux that looks like realtime

--- chatgpt-clone/main.py
+++ chatgpt-clone/main.py
@@ -24,13 +24,30 @@ if "session" not in st.session_state:
 session = st.session_state["session"]
 
 
+async def paint_history():
+    messages = await session.get_items()
+    for message in messages:
+        with st.chat_message(message["role"]):
+            if message["role"] == "user":
+                st.write(message["content"])
+            else:
+                if message["type"] == "message":
+                    st.write(message["content"][0]["text"])
+
+
+asyncio.run(paint_history())
+
+
 async def run_agent(message: str):
-    stream = Runner.run_streamed(agent, message, session=session)
-    async for event in stream.stream_events():
-        if event.type == "raw_response_event":
-            if event.data.type == "response.output_text.delta":
-                with st.chat_message("ai"):
-                    st.write(event.data.delta)
+    with st.chat_message("ai"):
+        text_placeholder = st.empty()
+        response = ""
+        stream = Runner.run_streamed(agent, message, session=session)
+        async for event in stream.stream_events():
+            if event.type == "raw_response_event":
+                if event.data.type == "response.output_text.delta":
+                    response += event.data.delta
+                    text_placeholder.write(response)
 
 
 prompt = st.chat_input("Write a message for your assistant")
